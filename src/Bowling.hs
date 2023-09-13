6a61129838af08bf05926a7f3f8ee3bbb62fea46
Alessandro Candolini <alessandrocandolini@gmail.com>
2023-09-13 23:41:30 +0100

    renaming

--- src/Bowling.hs
+++ src/Bowling.hs
@@ -7,21 +7,20 @@ import Control.Applicative (liftA2)
 
 newtype Score = Pin Int deriving (Eq)
    deriving (Show, Read, Ord, Num) via Int
-   deriving (Semigroup, Monoid) via Sum Int
+   deriving (Semigroup, Monoid) via Data.Monoid.Sum Int
 
 data FrameResult = Strike | Spares | KnockedDown Score Score deriving (Eq,Show)
 
-score :: NonEmpty FrameResult -> Score
-score = liftA2 (+) individualScores bonuses
+totalScore :: NonEmpty FrameResult -> Score
+totalScore = liftA2 (+) scores bonuses
 
+scores :: NonEmpty FrameResult -> Score
+scores = foldMap score
 
-individualScores :: NonEmpty FrameResult -> Score
-individualScores = foldMap individualScore
-
-individualScore :: FrameResult -> Score
-individualScore Strike = 10
-individualScore Spares = 10
-individualScore (KnockedDown s1 s2) = s1 + s2
+score :: FrameResult -> Score
+score Strike = 10
+score Spares = 10
+score (KnockedDown s1 s2) = s1 + s2
 
 bonuses :: NonEmpty FrameResult -> Score
 bonuses = const 0
