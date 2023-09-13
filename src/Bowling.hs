a0476d24b12bc3e4ae4164ca627093f4f9ca7167
Alessandro Candolini <alessandrocandolini@gmail.com>
2023-09-14 00:19:11 +0100

    example and property tests for non-bonus cases

--- src/Bowling.hs
+++ src/Bowling.hs
@@ -5,22 +5,25 @@ import Data.Monoid (Sum)
 import Data.List.NonEmpty
 import Control.Applicative (liftA2)
 
-newtype Score = Pin Int deriving (Eq)
+pins :: Num a => a
+pins = 10
+
+newtype Score = Score Int deriving (Eq)
    deriving (Show, Read, Ord, Num) via Int
    deriving (Semigroup, Monoid) via Data.Monoid.Sum Int
 
-data FrameResult = Strike | Spares | KnockedDown Score Score deriving (Eq,Show)
+data Outcome = Strike | Spares | KnockedDown Score Score deriving (Eq,Show)
 
-totalScore :: NonEmpty FrameResult -> Score
+totalScore :: NonEmpty Outcome -> Score
 totalScore = liftA2 (+) scores bonuses
 
-scores :: NonEmpty FrameResult -> Score
+scores :: NonEmpty Outcome -> Score
 scores = foldMap score
 
-score :: FrameResult -> Score
-score Strike = 10
-score Spares = 10
+score :: Outcome -> Score
+score Strike = pins
+score Spares = pins
 score (KnockedDown s1 s2) = s1 + s2
 
-bonuses :: NonEmpty FrameResult -> Score
+bonuses :: NonEmpty Outcome -> Score
 bonuses = const 0
--- test/BowlingSpec.hs
+++ test/BowlingSpec.hs
@@ -1,15 +1,34 @@
+{-# LANGUAGE OverloadedLists #-}
 module BowlingSpec where
 
 import Bowling
 import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck.Property
+import Test.QuickCheck
+import Data.List.NonEmpty
+
+scoreGen :: Score -> Gen Score
+scoreGen (Score max) = Score <$> chooseInt (0, max)
+
+knockedDownGen :: Gen Outcome
+knockedDownGen = do
+   r1 <- scoreGen (pins -2)
+   r2 <- scoreGen (r1 - 1)
+   return $ KnockedDown r1 r2
+
+knockedDownsGen :: Gen (NonEmpty Outcome)
+knockedDownsGen = (:|) <$> knockedDownGen <*> listOf knockedDownGen
+
+frameResultGen ::  Gen Outcome
+frameResultGen = oneof [knockedDownGen, pure Strike, pure Spares]
+
 
 spec :: Spec
-spec = describe "Simple test" $ do
+spec = describe "Bowling" $ do
+  describe "Score evaluation" $ do
+     it "example without bonuses" $
+        totalScore [(KnockedDown 5 3), (KnockedDown 2 3), (KnockedDown 0 1 )] `shouldBe` 14
 
-     it "example-based unit test" $
-        1 `shouldBe` 1
-
-     prop "property-based unit test" $
-        \l -> reverse ( reverse l ) == ( l::[Int])
+     prop "when outcomes are always partial knock downs, total scores are just the normal score " $
+      forAll knockedDownsGen $ \ s -> totalScore s == scores s
